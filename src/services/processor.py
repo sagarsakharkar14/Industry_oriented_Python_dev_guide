e9655807470d962c44ad908b3b557ef9142b7b16
sagarsakharkar14 <sagarsakharkar14@yahoo.com>
2025-09-20 18:23:58 +0530

    Resu

--- src/services/processor.py
+++ src/services/processor.py
@@ -1,5 +1,9 @@
+from src.utils.helpers import log
+
+
 def process_data(data: list[int]) -> list[int]:
     """
     Doubles each number in the list and returns only even results.
     """
-    return [x * 2 for x in data if (x * 2) % 2 == 0]
+    log(f"Processing data: {data}")
+    return [x * 2 for x in data if (x) % 2 == 0]
---
+++ src/utils/helpers.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def log(message: str):
+    logging.info(message)
